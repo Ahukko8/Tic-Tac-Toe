ff20613cd8600d090a776d68a9b74f8b14c97080
Ahukko8 <ahukko88@gmail.com>
2023-07-27 09:02:17 +0500

    start page designed

---
+++ script.js
@@ -0,0 +1,9 @@
+// Animation
+gsap.fromTo(".heading",  {y: -400}, { y: 40},{delay: 1},{ease: "in"});
+gsap.fromTo(".start-btn",  {y: -200}, { y: 50},{delay: 1}, {ease: "in"});
+
+
+
+// start-btn
+
+
