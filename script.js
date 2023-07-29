cd1dda767dc67d73c7155e4271a555b9eff40db5
Ahukko8 <ahukko88@gmail.com>
2023-07-29 10:17:57 +0500

    creating game board

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ gsap.fromTo(".start-btn",  {y: -200}, { y: 50},{delay: 1}, {ease: "in"});
 
 
 
-// start-btn
 
 
+let startButton = document.getElementById("startButton");
+let gameBoard = document.getElementById("container");
+let homePage = document.getElementById("homePage");
+let restartButton = document.getElementById("restartButton")
+
+
+startButton.onclick = function () {
+    container.style.display = "block";
+    homePage.style.display = "none";
+  }
+
+  
+  window.onclick = function (event) {
+    if (event.target == gameBoard) {
+      container.style.display = "none";
+    }
+  }
+  
